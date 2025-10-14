f8c860a30da5b5c7eea4723cfb62315a351449e3
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-10-14 18:16:09 +0000

    Blog 06.4

---
+++ DailyUpdate/Blog_06.4.js
@@ -0,0 +1,24 @@
+//Find First N Armstrong Numbers
+let count = 0;
+let N = 5
+
+for (let num = 1; true; num++) {
+    let temp = num;
+    let sum = 0;
+    let temp1 = num;
+
+    while (temp1 > 0) {
+        let rem = temp1 % 10;
+        let cube = Math.pow(rem, 3);
+        sum += cube;
+        temp1 = Math.trunc(temp1 / 10);
+    }
+    if (temp === sum) {
+        console.log(temp);
+        
+        count++
+    }
+    if (count === N) {
+        break;
+    }
+}
