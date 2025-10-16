2a578d49c20ac6557e9255353071d8def413aa80
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-10-16 18:47:59 +0000

    pattern update

---
+++ DailyUpdate/Blog_8.9.js
@@ -0,0 +1,14 @@
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let res = "";
+    for (let s = 1; s <= n - i; s++) {
+        res += "  ";
+    }
+    for (let j = 1; j <= i; j++) {
+        res += j + " ";
+    }
+    for (let k = i-1; k >= 1; k--) {
+        res += k+ " ";
+    }
+    console.log(res)
+}
