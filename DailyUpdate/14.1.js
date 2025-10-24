7769966d313a4f6a67dcb16883ac91d2a825a91f
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-10-24 15:42:41 +0000

    update Patterns

---
+++ DailyUpdate/14.1.js
@@ -0,0 +1,46 @@
+let n = 3;
+
+// Top half
+for (let i = 1; i <= n; i++) {
+  let res = "";
+
+  // spaces
+  for (let s = 1; s <= n - i; s++) {
+    res += " ";
+  }
+
+  // increasing numbers
+  for (let j = 1; j <= i; j++) {
+    res += j;
+  }
+
+  // decreasing numbers
+  for (let j = i - 1; j >= 1; j--) {
+    res += j;
+  }
+
+  console.log(res);
+}
+
+// Bottom half (mirror)
+for (let i = n - 1; i >= 1; i--) {
+  let res = "";
+
+  // spaces
+  for (let s = 1; s <= n - i; s++) {
+    res += " ";
+  }
+
+  // increasing numbers
+  for (let j = 1; j <= i; j++) {
+    res += j;
+  }
+
+  // decreasing numbers
+  for (let j = i - 1; j >= 1; j--) {
+    res += j;
+  }
+
+  console.log(res);
+}
+
