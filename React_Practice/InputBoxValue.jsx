92756d215318ce7d20b67d718316e01bbdae4ed5
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-11-29 17:10:07 +0000

    creating Input Box Value  App

---
+++ React_Practice/InputBoxValue.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function InputBox() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}  // update state
+      />
+
+      <h3>You typed: {text}</h3>  {/* show live text */}
+    </div>
+  );
+}
+
+export default InputBox;
