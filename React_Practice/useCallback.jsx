27e231df1e46267f8b539f936436265c710229bf
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-11-30 16:34:10 +0000

    using useCallback

---
+++ React_Practice/useCallback.jsx
@@ -0,0 +1,17 @@
+import { useState, useCallback } from "react";
+
+function CallbackExample() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    console.log("Clicked!");
+  }, []);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  );
+}
