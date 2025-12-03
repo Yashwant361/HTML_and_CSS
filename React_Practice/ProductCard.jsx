703dd304866f469c730ab20bd7a1c1611e305a83
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-12-03 16:13:01 +0000

    React checkBox

---
+++ React_Practice/ProductCard.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function ProductCard2() {
+
+    const [products, setProducts] = useState([
+        {
+            id: 101,
+            name: 'Maggie',
+            price: 40,
+            imgUrl: 'https://images.unsplash.com/photo-1589302168068-964664d93dc0'
+        },
+        {
+            id: 102,
+            name: 'Aloo Paratha',
+            price: 60,
+            imgUrl: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c'
+        },
+        {
+            id: 103,
+            name: 'Pizza',
+            price: 180,
+            imgUrl: 'https://images.unsplash.com/photo-1594007654729-407eedc4be51'
+        }
+    ]);
+
+    return (
+        <div>
+            <h2>Product List</h2>
+
+            {/* for...of loop */}
+            {(() => {
+                const items = [];   // yaha const sahi hai (array ko reassign nahi kar rahe)
+                
+                for (const p of products) {
+                    const { id, name, price, imgUrl } = p;
+
+                    items.push(
+                        <div key={id} style={{border:'1px solid black', margin:'10px', padding:'10px'}}>
+                            <img src={imgUrl} width="150" />
+                            <h3>{name}</h3>
+                            <p>₹{price}</p>
+                        </div>
+                    );
+                }
+
+                return items;
+            })()}
+        </div>
+    );
+}
+
+export default ProductCard2;
