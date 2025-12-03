c3aabd864cf5842ed1c465a7270cd8d999a2407c
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-12-03 16:14:30 +0000

    update

--- DailyUpdate/Blog_11.4.js
+++ DailyUpdate/Blog_11.4.js
@@ -1,7 +1,7 @@
 let n = 4;
 let count = 1;
 for (let i = 1; i <= n; i++) {
-    let res = ""git add  DailyUpdate/Blog_11.3.js && git commit -m 'Correction ' && git push
+    let res = ""
     for (let j = 1; j <= i; j++) {
         res += count++ +" "
     }
