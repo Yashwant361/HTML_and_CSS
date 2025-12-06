a3fe98a460616b3b676d02bfe970f0e27719feb7
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-12-06 16:02:21 +0000

    Child  Component

---
+++ MoviesCard/MovieDesc.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MovieDesc({ desc }) {
+  return (
+    <div className="border p-3 mt-2 bg-light rounded">
+      <h5>Description</h5>
+      <p>{desc}</p>
+    </div>
+  );
+}
+
+export default MovieDesc;
