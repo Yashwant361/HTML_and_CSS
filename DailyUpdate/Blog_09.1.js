96157d4ab799a406253dac1b151d44677186c967
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-12-09 16:50:29 +0000

    update

--- DailyUpdate/Blog_09.1.js
+++ DailyUpdate/Blog_09.1.js
@@ -2,7 +2,7 @@ let n = 5;
 for (let i = 1; i <= n; i++) {
     let res = "";
     for (let j = 1; j <=n-i+1; j++) {
-           res+=j+"  ";
+           res+=j+" ";
     }
     console.log(res)
     
