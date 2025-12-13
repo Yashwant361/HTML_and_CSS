e4dab7c454bf6ef043cbd681506ca4987e48e159
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-12-13 16:49:39 +0000

    correct

--- DailyUpdate/13.5.js
+++ DailyUpdate/13.5.js
@@ -1,7 +1,7 @@
 let n = 5;
 for (let i = 1; i <= n; i++) {
     let res = "";
-    for (let j = 0; j < i; j++){
+    for (let j = 0; j <i; j++){
         res+=String.fromCharCode(69-j)+" ";
     }
     console.log(res);
